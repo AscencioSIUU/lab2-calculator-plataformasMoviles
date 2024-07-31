ddd59c6a1b4ed34ee6bfefe1c088f545d57c9153
AscencioSIUU <131807749+AscencioSIUU@users.noreply.github.com>
2024-07-30 22:29:37 -0600

    Console working

--- lab2Calculator/src/main/kotlin/Main.kt
+++ lab2Calculator/src/main/kotlin/Main.kt
@@ -1,12 +1,29 @@
 package org.example
 
+import java.beans.Expression
+
 fun main() {
+    private val validator = ExpressionValidator()
+    private val converter = ExpressionConverter()
     println("Bienvenido a la Mejor Calculadora del Mundo")
     var cycle = true
 
     while (cycle){
         print("Ingrese su cadena numeros a evaluar: ")
         val input = readLine()!!
+
+        val areCharactersValid = validator.validateCharacters(input)
+        val areParenthesesValid = validator.validateParenthesis(input)
+
+        if(!areParenthesesValid){
+            println("Expresion contiene paréntesis no balanceados")
+        }
+
+        val postFixString = converter.infixToPostfix(areParenthesesValid)
+
+
+
+
 
     }
 }
